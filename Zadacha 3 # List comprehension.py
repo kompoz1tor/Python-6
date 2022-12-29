5fd7bf81fc263c333240abc66ddf4584f676cd06
Zhavoronkov Sergey <zhavoronkov-s@mail.ru>
2022-12-29 16:30:03 +0300

    list comprehension

--- Zadacha 3 # List comprehension.py
+++ Zadacha 3 # List comprehension.py
@@ -8,13 +8,13 @@ n = int(input('Введите кол-во элементов списка: '))
 '''
 list = []
 for i in range(5):
-    list.append(randint(0, 10))
+    list.append(randint(0, 10))
 print(f'Исходный список: {list}')
 n = len(list)
 for i in range(n):
-    j = random.randint(0, n-1)
-    element = list.pop(j)
-    list.append(element)
+    j = random.randint(0, n-1)
+    element = list.pop(j)
+    list.append(element)
 print(f'Перемешанный список: {list}')
 
 '''
@@ -24,7 +24,7 @@ list = [x for x in range(n)]
 print(f'Исходный список: {list}')
 
 for i in range(n):
-    j = random.randint(0, n-1)
-    element = list.pop(j)
-    list.append(element)
+    j = random.randint(0, n-1)
+    element = list.pop(j)
+    list.append(element)
 print(f'Перемешанный список: {list}')
